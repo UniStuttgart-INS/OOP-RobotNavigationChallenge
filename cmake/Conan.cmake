267e5f915d312a3abf4a157c6fe61b0f3cb4638b
Thomas Topp <thomas@topp.cc>
2022-05-16 11:20:06 +0200

    Conan cmake update to 18.1 (support for MSVC 2022)

--- cmake/Conan.cmake
+++ cmake/Conan.cmake
@@ -1,16 +1,11 @@
 macro(run_conan)
   # Download automatically, you can also just copy the conan.cmake file
   if(NOT EXISTS "${CMAKE_BINARY_DIR}/conan.cmake")
+
     message(STATUS "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
-    file(DOWNLOAD "https://github.com/conan-io/cmake-conan/raw/v0.15/conan.cmake"
-         "${CMAKE_BINARY_DIR}/conan.cmake" STATUS status)
-
-    list(GET status 0 error_code)
-
-    if(error_code)
-      message(STATUS "Could not download Conan Cmake file, using local backup")
-      file(COPY "cmake/Conan-v0.15/conan.cmake" DESTINATION "${CMAKE_BINARY_DIR}")
-    endif()
+    file(DOWNLOAD "https://raw.githubusercontent.com/conan-io/cmake-conan/0.18.1/conan.cmake"
+                "${CMAKE_BINARY_DIR}/conan.cmake"
+                TLS_VERIFY ON)
   endif()
 
   include(${CMAKE_BINARY_DIR}/conan.cmake)
